3a85e0c636da2481ad8bc57c762a185708c77158
Louis-Guiniot <61980982+Louis-Guiniot@users.noreply.github.com>
2021-01-21 11:30:27 +0100

    aggiunto ngrx

--- gestionalelsg/src/app/app.module.ts
+++ gestionalelsg/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { StoreModule } from '@ngrx/store';
 
 
 @NgModule({
@@ -13,6 +14,7 @@ import { AppRoutingModule } from './app-routing.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    StoreModule.forRoot({}, {}),
   ],
   providers: [],
   bootstrap: [AppComponent]
