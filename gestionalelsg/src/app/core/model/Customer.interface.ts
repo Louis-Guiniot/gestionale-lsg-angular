d0d1d60668d77fbfb940120d421a2817f6207316
Pietro Locatelli <pietro.locatelli@stu.fondazionejobsacademy.org>
2021-02-06 10:38:08 +0100

    customer

--- gestionalelsg/src/app/core/model/Customer.interface.ts
+++ gestionalelsg/src/app/core/model/Customer.interface.ts
@@ -1,6 +1,8 @@
 export interface Customer{
     id?: number;
-    name: string;
-    surname: string;
     email:string;
+    partivaIva:string;
+    ragioneSociale:string;
+    residenza:string;
+    sede:string;
 }
